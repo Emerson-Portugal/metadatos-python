61e2582ee3c8fff45c39ecb24164ba66e5257bc3
Emersoncrp <emersoncrp@gmail.com>
2023-02-13 22:31:35 -0500

    Version1

---
+++ prueba.py
@@ -0,0 +1,25 @@
+import exifread
+import urllib.parse
+
+# Open the image file for reading (binary mode)
+f = open("ggh.jpg", "rb")
+
+# Return Exif tags
+tags = exifread.process_file(f)
+
+# Get latitude and longitude values from the metadata
+lat = tags.get("GPS GPSLatitude")
+lon = tags.get("GPS GPSLongitude")
+
+# Construct a Google Maps URL using the latitude and longitude values
+url = f"https://maps.google.com/?q={lat},{lon}"
+
+# Write the metadata and URL to a text file
+with open('metadata.txt', 'w') as file:
+    for tag in tags.keys():
+        if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
+            file.write(f"{tag:25}: {tags[tag]}\n")
+    file.write(f"\nGoogle Maps URL: {url}\n")
+
+# Close the file
+f.close()
